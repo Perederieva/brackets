524a709c331e8c7239bfa2ce453adf2812bdf5dd
Perederieva <alin4ik.ua@gmail.com>
2019-10-06 13:39:02 +0300

    feat: add solutiob

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    var splitStr = str.split('');
+
+    for (let i = 0; i <= splitStr.length; i++) {
+        for (let k = 0; k < splitStr.length; k++) {
+            var el1 = splitStr[k];
+            var el2 = splitStr[k + 1];
+            for (let j = 0; j < bracketsConfig.length; j++) {
+                if ((el1 === bracketsConfig[j][0]) && (el2 === bracketsConfig[j][1])) {
+                    splitStr.splice(k, 2);
+                    break;
+                }
+            }
+        }
+    }
+
+    return splitStr.length === 0;
+};
